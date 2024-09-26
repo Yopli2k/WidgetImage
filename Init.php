1fd09071628be76cfe8a9777e5cc79e6f4b10f74
José Antonio Cuello Principal <yopli2000@gmail.com>
2024-09-26 09:46:14 +0200

    initial commit

---
+++ Init.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * This file is part of WidgetImage plugin for FacturaScripts.
+ * FacturaScripts Copyright (C) 2015-2024 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * WidgetRichText Copyright (C) 2024-2024 Jose Antonio Cuello Principal <yopli2000@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace FacturaScripts\Plugins\WidgetImage;
+
+use FacturaScripts\Core\Template\InitClass;
+
+/**
+ * Description of Init
+ *
+ * @author Jose Antonio Cuello Principal <yopli2000@gmail.com>
+ */
+class Init extends InitClass
+{
+
+    /**
+     * Code to load every time FacturaScripts starts.
+     */
+    public function init(): void
+    {
+    }
+
+    /**
+     * Code to load every time the plugin is enabled or updated.
+     */
+    public function update(): void
+    {
+    }
+
+    /**
+     * Code that is executed when uninstalling a plugin.
+     */
+    public function uninstall(): void
+    {
+    }
+}
